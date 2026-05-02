23f1dec2d6d4bf33ed06bb9cd54422958eed9bfc
TovDinin <icrealt@gmail.com>
2026-05-03 00:38:01 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'tobolsk-quest-v1';
+const CACHE_NAME = 'questhub-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/cities.json',
   '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
@@ -23,9 +24,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
       );
     })
   );
